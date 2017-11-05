3157e8a7efe47d37fc13a51ab354add002b459d6
Vy Nguyen <ntvy95@gmail.com>
2017-11-05 10:51:35 +0700

    Update managelinks.js

--- js/managelinks.js
+++ js/managelinks.js
@@ -77,7 +77,7 @@ app.controller('managelinks_ctrl', function($scope, $location, $interval, $timeo
           playerVars: {
             'controls': 0,
             'showinfo': 0,
-            'origin': 'https://www.youtube.com'
+            'origin': 'https://ntvy95.github.io'
           },
           events: {
             'onReady': function(event) {
